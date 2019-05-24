3e6e819985fc19378f1aa2aa108d42ebc6f7f926
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-05-24 17:52:36 +0200

    fix bar menu and about section in rwd

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ document.querySelector('.bar').addEventListener('click', displayMenu);
 function displayMenu() {
     document.querySelector('.navigation').classList.toggle('show');
     document.querySelector('.hero').classList.toggle('move');
+    document.querySelector('.welcome').classList.toggle('no-show');
 }
